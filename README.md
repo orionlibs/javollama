484438625b57a646515bf688bb315abd7d435fab
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-08-25 17:44:01 +0100

    added test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 JavOllama
 Java implementation of the Ollama project 
+
+add this VM option to the JVM when you run the project:
+--enable-preview --add-modules jdk.incubator.vector
+
+example call of the program:
+--model=D:/downloads/Meta-Llama-3.1-8B-Instruct-Q4_0.gguf --prompt="Why is the sky blue?"
---
+++ src/test/java/io/github/orionlibs/javollama/MainLlama31Test.java
@@ -0,0 +1,54 @@
+package io.github.orionlibs.javollama;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.github.orionlibs.javollama.log.ListLogHandler;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("testing")
+@TestInstance(Lifecycle.PER_CLASS)
+//@Execution(ExecutionMode.CONCURRENT)
+//@RunWith(JUnitPlatform.class)
+public class MainLlama31Test
+{
+    private ListLogHandler listLogHandler;
+    //@Autowired
+    //private NewClass newClass;
+
+
+    @BeforeEach
+    void setUp()
+    {
+        listLogHandler = new ListLogHandler();
+        //NewClass.addLogHandler(listLogHandler);
+    }
+
+
+    @AfterEach
+    public void teardown()
+    {
+        //NewClass.removeLogHandler(listLogHandler);
+    }
+
+
+    @Test
+    @Disabled
+    void test_main() throws Exception
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+        MainLlama31.main(new String[] {"--model=src/test/resources/io/github/orionlibs/javollama/models/Meta-Llama-3.1-8B-Instruct-Q4_0.gguf", "--prompt=\"Why is the sky blue? Answer in no more than 12 words. Start your answer with the words \"The sky appears blue due to\"\""});
+        System.setOut(originalOut);
+        String capturedOutput = outputStream.toString();
+        assertTrue(capturedOutput.startsWith("The sky appears blue due to"));
+    }
+}
